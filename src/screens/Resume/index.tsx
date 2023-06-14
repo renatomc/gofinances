64bd861228e3595bc992cda9a737fb4ee3c56718
= <=>
2023-06-13 23:46:07 -0300

    Commit sem a parte dos graficos e sem resumo

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { Container, Header, Title } from './styles';
+import { Container, Header, Title, Content } from './styles';
 import HistoryCard from '../../components/HistoryCard';
+import { categories } from '../../utils/categories';
+
+export interface TransactionData {
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+ };
+
+ export interface CategoryData  {
+  name: string;
+  total: number;
+  totalFormatted: string;
+  color: string;
+ }
+
 
 const Resume: React.FC = () => {
+  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+  async function loadData() {
+    const dataKey = '@gofinances:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+
+    const responseFormatted: TransactionData[] = response ? JSON.parse(response) : [];
+
+    const expensives = responseFormatted.filter(expensive => expensive.type === 'negative');
+
+    let totalByCategory: CategoryData[] = [];
+
+    categories.forEach(category => {
+      let categorySum: number = 0;
+
+      expensives.forEach(expensive => {
+        if(expensive.category === category.key) {
+          categorySum += Number(expensive.amount); 
+        }
+      });
+
+      if(categorySum > 0) {
+        const totalFormatted = categorySum
+        .toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        });
+
+        totalByCategory.push({
+          name: category.name,
+          total: categorySum,
+          totalFormatted,
+          color: category.color
+        })
+      }
+    });
+
+    setTotalByCategories(totalByCategory);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [])
+
   return (
     <Container>
       <Header>
@@ -11,9 +73,13 @@ const Resume: React.FC = () => {
           Resumo por categoria
         </Title>
       </Header>
-      <HistoryCard title='Teste' amount='R$ 2.99' color='red' />
-      <HistoryCard title='Teste 2' amount='R$ 12.99' color='blue' />
-      <HistoryCard title='Teste 3' amount='R$ 22.99' color='#f1f' />
+      <Content>
+      {totalByCategories.length > 0 && (
+        totalByCategories.map(totalByCategory => (
+          <HistoryCard key={totalByCategory.name} title={totalByCategory.name} amount={totalByCategory.totalFormatted} color={totalByCategory.color} />
+         ))
+        )}
+      </Content>
     </Container>
   )
 }
